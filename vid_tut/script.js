1ad91d3ec06ccb3e0af817b43b73288ab0cc710a
PNK123MH <philimonakidusan@gmail.com>
2026-08-14 16:18:14 +0300

    second commit

---
+++ vid_tut/script.js
@@ -0,0 +1,24 @@
+let x=1;
+let y=2;
+let z=x+y;
+console.log(z)
+function add(a,b){
+    let z=a+b;
+    console.log(z)
+}
+add(1,2);
+add(2,3);
+
+for(let i=1;i<=5;i++){
+    console.log(5)
+}
+function age(x){
+    if(x>=18){
+        console.log("ADULT")
+    }
+    else{
+        console.log("CHILD")
+    }
+}
+age(10);
+age(45);
